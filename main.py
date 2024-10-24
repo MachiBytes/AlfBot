d5aa0fa2714542a82fe2d4e9ef71f2b526af7987
EC2 Default User <ec2-user@ip-172-26-9-46.ap-southeast-1.compute.internal>
2024-10-24 14:42:57 +0000

    Testing command

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import settings
 import discord
 from discord.ext import commands
 
-logger = settings.logging.getLogger("bot")
-
 def run():
-    intents = discord.Intents.default()
+    intents = discord.Intents.all()
 
     bot = commands.Bot(command_prefix="!", intents=intents)
     
     @bot.event
     async def on_ready():
-        logger.info(f"User: {bot.user} ({bot.user.id})")
-        print(bot.user)
+        print("Bot is running.")
+
+    @bot.command()
+    async def test(ctx, arg):
+        await ctx.send(arg)
     
     bot.run(settings.DISCORD_API_TOKEN, root_logger=True)
 
