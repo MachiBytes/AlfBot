dd8665b5b68bce6941891fb97eaa3d078a9554e6
Mark Achiles <markachilesflores2004@gmail.com>
2024-10-24 22:30:16 +0800

    Testing lang, can reset

---
+++ README.md
@@ -0,0 +1,5 @@
+# To contribute
+- Clone the repository
+- Create a virtual environment
+- Install `requirements.txt`
+- Copy `.env.example` into a file called `.env` and fill in the values. Contact an administrator for the values.
---
+++ settings.py
@@ -0,0 +1,56 @@
+import os
+import logging
+from logging.config import dictConfig
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_API_TOKEN = os.getenv("DISCORD_API_TOKEN")
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disabled_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard": {
+            "format": "%(levelname)-10s - %(name)-15s : %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard"
+        },
+        "console2": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standard"
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/infos.log",
+            "mode": "w"
+        },
+    },
+    "loggers": {
+        "bot": {
+            "handler": ["console"],
+            "level": "INFO",
+            "propagate": False
+        },
+        "discord": {
+            "handler": ["console2", "file"],
+            "level": "INFO",
+            "propagate": False
+        }
+    }
+}
+
+dictConfig(LOGGING_CONFIG)
+
+if __name__ == "__main__":
+    pass
